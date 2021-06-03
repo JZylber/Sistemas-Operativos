154ace8f8a2ea5943605000c3a24d1dcd9138a0f
Nicolas Bukovits <nicobuk@gmail.com>
2021-06-03 18:11:17 -0300

    taller de fs

---
+++ TallerFS/cmake-build-debug/CMakeFiles/TallerFS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TallerFS.dir/main.cpp.o"
+  "TallerFS"
+  "TallerFS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TallerFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
